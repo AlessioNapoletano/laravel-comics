940856aaf7f5636500cd75518de6c3e072370186
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-08 19:48:32 +0100

    the number °1 is back

--- config/main-nav.php
+++ config/main-nav.php
@@ -7,20 +7,20 @@ return [
             "description" => "DIGITAL COMICS"
         ],
         [
-            "img" => "resources/assets/img/buy-comics-digital-comics.png",
+            "img" => "resources/assets/img/buy-comics-merchandise.png",
             "description" => "DC MERCHANDISE"
         ],
         [
-            "img" => "resources/assets/img/buy-comics-digital-comics.png",
-            "description" => "DIGITAL COMICS"
+            "img" => "resources/assets/img/buy-comics-subscriptions.png",
+            "description" => "SUBSCRIPTION"
         ],
         [
-            "img" => "resources/assets/img/buy-comics-digital-comics.png",
-            "description" => "DIGITAL COMICS"
+            "img" => "resources/assets/img/buy-comics-shop-locator.png",
+            "description" => "COMIC SHOP LOCATOR"
         ],
         [
-            "img" => "resources/assets/img/buy-comics-digital-comics.png",
-            "description" => "DIGITAL COMICS"
+            "img" => "resources/assets/img/buy-dc-power-visa.svg",
+            "description" => "DC POWER VISA"
         ],
     ]
 ];
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,36 @@
-<p> questo è il footer </p>
+<section class="main-footer">
+    <div class="left">
+        <!--DC Comics-->
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+
+        <!--Shop-->
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+
+         <!--DC-->
+         <nav>
+            <ul>
+
+            </ul>
+        </nav>
+
+        <!--Sites-->
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+    </div>
+    <div class="right"></div>
+</section>
+
+<section class="end-footer">
+
+</section>
