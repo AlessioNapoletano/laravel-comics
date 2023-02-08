4fdbc6854470887a72cc89ff8cf617966ef1a63f
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-02-08 20:29:44 +0100

    the last commit for real

--- config/footer-nav.php
+++ config/footer-nav.php
@@ -7,7 +7,31 @@ return [
             "title" => "DC COMICS",
             "links" => ["Characters" , "Comics" , "Movies" , "TV", "Games", "Videos", "News"]
         ],
-        
+        [
+            "title" => "SHOP",
+            "links" => ["Shop DC" , "Shop DC Collectibles"]
+        ],
+        [
+            "title" => "DC",
+            "links" => 
+            [
+                "Terms OF Use" ,
+                "Privacy policy (New)", 
+                "Ad Choices",
+                "Advertising",
+                "Jobs",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificates",
+                "Ratings",
+                "Shop Help",
+                "Contact US",
+            ]
+        ],
+        [
+            "title" => "SITES",
+            "links" => ["DC" , "MAD Magazine" , "DC Kids", "DC Universe", "DC Power Visa"]
+        ]
     ]
 ];
 
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,22 +1,25 @@
 <section class="main-footer">
-    <div class="left">
-        <nav>
-            @foreach ($footerNavElement as $navElement)
-                <ul class="text-light">
-                    <h2>
-                        {{ $navElement['title'] }}
-                    </h2>
-                    @foreach ($navElement['links'] as $link )
-                    <li>
-                        {{ $link }}
-                    </li>
-                    @endforeach
-                    
-                </ul>
-            @endforeach
-        </nav>
-       
-    <div class="right"></div>
+    <div class="container d-flex">
+        <div class="left">
+            <nav class="d-flex pt-5">
+                @foreach ($footerNavElement as $navElement)
+                    <ul class="text-light list-unstyled me-3">
+                        <h2 class="fw-bold">
+                            {{ $navElement['title'] }}
+                        </h2>
+                        @foreach ($navElement['links'] as $link )
+                        <li>
+                            {{ $link }}
+                        </li>
+                        @endforeach
+                        
+                    </ul>
+                @endforeach
+            </nav>
+        </div>
+    </div>
+    
+    
 
 </section>
 
